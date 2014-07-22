6b8086a4eff2151c59bac0b956b6b61c3dd58ace
Arnaud Ligny <arnaud@ligny.org>
2014-07-23 00:31:09 +0200

    Update EventController.php

--- src/app/code/community/Narno/Mailjet/controllers/EventController.php
+++ src/app/code/community/Narno/Mailjet/controllers/EventController.php
@@ -2,7 +2,7 @@
 /**
  * @category   Narno
  * @package    Narno_Mailjet
- * @copyright  Copyright (c) 2011-2013 Narno (http://narno.com)
+ * @copyright  Copyright (c) 2011-2014 Narno (http://narno.com)
  * @author     Arnaud Ligny <contact@narno.com>
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -10,8 +10,8 @@
 class Narno_Mailjet_EventController
     extends Mage_Core_Controller_Front_Action
 {
-	public function indexAction()
-	{
+    public function indexAction()
+    {
         if ($this->getRequest()->isPost()) {
             $websiteId = Mage::app()->getStore()->getWebsiteId();
             $subscriber = Mage::getModel('newsletter/subscriber')
@@ -33,8 +33,8 @@ class Narno_Mailjet_EventController
             $this->getResponse()->setHttpResponseCode(405);
             $this->getResponse()->setBody(
                 '<h1>Narno Mailjet module event trigger API endpoint</h1>
-                 <p>Method not allowed</p>'
+                <p>Method not allowed</p>'
             );
         }
-	}
+    }
 }
